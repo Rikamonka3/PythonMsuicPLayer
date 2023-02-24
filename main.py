70f1469ae84b588d76c147808dd015070a5ad1f7
FemboyRika <105397375+Rikamonka3@users.noreply.github.com>
2023-02-23 21:59:08 -0700

    Update

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+import fnmatch
+import os
+from pygame import mixer
+
+canvas = tk.Tk()
+canvas.title("Music Player")
+canvas.geomentry("600x800")
+canvas.config(bg = 'black')
+
+canvas.mainloop()
