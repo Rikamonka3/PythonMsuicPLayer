89c448fc3b41f437af22deb208fa65716054389b
FemboyRika <105397375+Rikamonka3@users.noreply.github.com>
2023-02-24 20:17:35 -0700

    Done and Done

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ canvas.config(bg = 'black')
 rootpath = "C:\\Users\dillo\Documents\GitHub\PythonMsuicPLayer\Msuic"
 pattern = "*.mp3"
 
+mixer.init()
+
 prev_img = tk.PhotoImage(file = "previous_img.png")
 next_img = tk.PhotoImage(file = "next_img.png")
 play_img = tk.PhotoImage(file = "play_img.png")
@@ -21,6 +23,47 @@ def select():
     label.config(text = listBox.get("anchor"))
     mixer.music.load(rootpath + "\\" + listBox.get("anchor"))
     mixer.music.play()
+
+def stop():
+    mixer.music.stop()
+    listBox.select_clear('active')
+
+def play_next():
+    next_song = listBox.curselection()
+    next_song = next_song[0] + 1
+    next_song_name = listBox.get(next_song)
+    label.config(text = next_song_name)
+
+    mixer.music.load(rootpath + "\\" + next_song_name)
+    mixer.music.play()
+
+    listBox.select_clear(0, 'end')
+    listBox.activate(next_song)
+    listBox.select_set(next_song)
+
+def  play_prev():
+    prev_song = listBox.curselection()
+    prev_song = prev_song[0] - 1
+    prev_song_name = listBox.get(prev_song)
+    label.config(text = prev_song_name)
+
+    mixer.music.load(rootpath + "\\" + prev_song_name)
+    mixer.music.play()
+
+    listBox.select_clear(0, 'end')
+    listBox.activate(prev_song)
+    listBox.select_set(prev_song)
+
+def pause_song():
+    if pauseButton["text"] == "Pause":
+        mixer.music.pause()
+        pauseButton["text"] = "play"
+    else:
+        mixer.mixer.unpause()
+        pauseButton["text"] = "Pause"
+
+
+
 
 
 
@@ -34,19 +77,19 @@ top = tk.Frame(canvas, bg = "black")
 top.pack(padx = 10, pady= 5, anchor = 'center')
 
 
-prevButton = tk.Button(canvas, text = "Prev", image = prev_img, bg = 'white', borderwidth = 0)
+prevButton = tk.Button(canvas, text = "Prev", image = prev_img, bg = 'white', borderwidth = 0,command=  play_prev)
 prevButton.pack(pady=15, in_= top, side= 'left')
 
-stopButton = tk.Button(canvas, text = "Stop", image = stop_img, bg = 'white', borderwidth = 0)
+stopButton = tk.Button(canvas, text = "Stop", image = stop_img, bg = 'white', borderwidth = 0, command = stop)
 stopButton.pack(pady=15, in_= top, side= 'left')
 
 playButton = tk.Button(canvas, text = "Play", image = play_img, bg = 'white', borderwidth = 0, command = select)
 playButton.pack(pady=15, in_= top, side= 'left')
 
-pauseButton = tk.Button(canvas, text = "Pause", image = pause_img, bg = 'white', borderwidth = 0)
+pauseButton = tk.Button(canvas, text = "Pause", image = pause_img, bg = 'white', borderwidth = 0, command = pause_song)
 pauseButton.pack(pady=1, in_= top, side= 'left')
 
-nextButton = tk.Button(canvas, text = "Next", image = next_img, bg = 'white', borderwidth = 0)
+nextButton = tk.Button(canvas, text = "Next", image = next_img, bg = 'white', borderwidth = 0, command = play_next)
 nextButton.pack(pady=15, in_= top, side= 'left')
 
 
